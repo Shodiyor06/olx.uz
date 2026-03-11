97bf8de2461283fba1896bbcc4a52e3765f744e7
Shodiyor06 <sohibotaqulov76@gmail.com>
2026-03-11 09:55:11 +0500

    d

--- products/models.py
+++ products/models.py
@@ -37,7 +37,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="active")
     region = models.CharField(max_length=100, blank=True)
     view_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
